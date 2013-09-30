5ec3047d424171d6edb75ae69576c205a2e94e40
Matt DuVall <mduvall89@gmail.com>
2013-09-30 00:12:43 -0700

    Curtain with DOM ordering

--- js/peeler.js
+++ js/peeler.js
@@ -1,4 +1,6 @@
 (function() {
+  var ABOVE = "1000";
+
   var root = this,
       Peeler = function() {},
       articles = document.querySelectorAll("article"),
@@ -15,15 +17,39 @@
 
     root.onscroll = function(event) {
       var yOffset = window.pageYOffset,
-          i = 0;
-      for (len = articles.length; i < len; i++) {
+          i = 0,
+          j = 0,
+          triggered = false,
+          below = 999,
+          curr,
+          len = articles.length;
+
+      for (; i < len; i++) {
         if (yOffset <= articleStates[i].max && yOffset >= articleStates[i].min) {
           articles[i].style["margin-top"] = -(yOffset-(articleStates[i].min)) + "px";
-          articles[i].style["z-index"] = "2";
-        } else {
-          articles[i].style["margin-top"] = "0px";
-          articles[i].style["z-index"] = "1";
+          articles[i].style["z-index"] = ABOVE;
+          triggered = true;
+          curr = i;
+          break;
         }
+      }
+
+      for (;j < len; j++) {
+        if (i !== curr) {
+          articles[i].style["margin-top"] = "0px";
+          articles[i].style["z-index"] = below--;
+        }
+        i++;
+        // Reset at rotation point
+        if (i == len) {
+          i = 0;
+        }
+      }
+
+      // Case where it goes above the fold for OSX trackpads
+      if (!triggered) {
+          articles[0].style["margin-top"] = "0px";
+          articles[0].style["z-index"] = ABOVE;
       }
     };
 
@@ -32,7 +58,10 @@
     // Set each of the articles according to the aspect ratio
     for (i = 0, len = articles.length; i < len; i++) {
       article = articles[i];
-      height = viewportWidth * aspectRatio;
+      // viewport with aspectRatio
+      //height = screen.height;
+
+      height = root.innerWidth * aspectRatio;
       article.style.height = height + "px";
       articleStates.push({min: bodyHeight, max: bodyHeight+height});
       bodyHeight += height;
