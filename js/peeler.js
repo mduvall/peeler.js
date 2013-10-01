61ba590b09aee287ea7b7d20307edf6d5d7a58be
Matthew DuVall <mduvall89@gmail.com>
2013-10-01 08:54:01 -0700

    Add data-height support for > screen.height

--- js/peeler.js
+++ js/peeler.js
@@ -61,8 +61,8 @@
     for (i = 0, len = articles.length; i < len; i++) {
       article = articles[i];
       // viewport with aspectRatio
-      height = screen.height;
-      article.style.height = height + "px";
+      height = article.getAttribute("data-height") || screen.height;
+      article.style.height = parseInt(height) + "px";
       articleStates.push({min: bodyHeight, max: bodyHeight+height});
       bodyHeight += height;
     }
